e014bba694d055fedddbc56d2803cef8f7f18bfb
Riku Rouvila <riku.rouvila@gmail.com>
2020-09-05 12:31:21 +0300

    add new columns to schema

--- schema.sql
+++ schema.sql
@@ -3,17 +3,18 @@ DROP TABLE IF EXISTS `labelled`;
 DROP TABLE IF EXISTS `predictions_table`;
 
 CREATE TABLE clips (url TEXT, start INT, end INT, mfcc TEXT, amplitude FLOAT, pitch FLOAT,
-       isHighlight BOOLEAN);
+       isHighlight BOOLEAN, pred_highlight_rf INT, prob_highlight_nn INT, pred_highlight_nn INT);
 
 CREATE TABLE labelled (url TEXT, start INT, end INT, mfcc TEXT, amplitude FLOAT, pitch FLOAT,
-       isHighlight BOOLEAN);
+       isHighlight BOOLEAN, pred_highlight_rf INT, prob_highlight_nn INT, pred_highlight_nn INT);
 
 CREATE TABLE predictions_table (isHighlight BOOLEAN, pred_highlight_rf INT, prob_highlight_nn INT, pred_highlight_nn INT);
 
 
+
+
 ALTER TABLE clips ADD word TEXT;
 ALTER TABLE labelled ADD word TEXT;
-
 
 ALTER TABLE clips ADD subjectivity TEXT, ADD polarity TEXT, ADD pred_excitement TEXT;
 ALTER TABLE labelled ADD subjectivity TEXT, ADD polarity TEXT, ADD pred_excitement TEXT;
