bebe6495ecf3dbf51c37f22a6e1dcdc70b76cf26
Riku Rouvila <riku.rouvila@gmail.com>
2020-09-05 12:07:13 +0300

    add new table to schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,18 @@
--- DROP TABLE IF EXISTS `clips`;
--- DROP TABLE IF EXISTS `labelled`;
+DROP TABLE IF EXISTS `clips`;
+DROP TABLE IF EXISTS `labelled`;
+DROP TABLE IF EXISTS `predictions_table`;
 
--- CREATE TABLE clips (url TEXT, start INT, end INT, mfcc TEXT, amplitude FLOAT, pitch FLOAT,
---        isHighlight BOOLEAN);
+CREATE TABLE clips (url TEXT, start INT, end INT, mfcc TEXT, amplitude FLOAT, pitch FLOAT,
+       isHighlight BOOLEAN);
 
--- CREATE TABLE labelled (url TEXT, start INT, end INT, mfcc TEXT, amplitude FLOAT, pitch FLOAT,
---        isHighlight BOOLEAN);
+CREATE TABLE labelled (url TEXT, start INT, end INT, mfcc TEXT, amplitude FLOAT, pitch FLOAT,
+       isHighlight BOOLEAN);
+
+CREATE TABLE predictions_table (isHighlight BOOLEAN, pred_highlight_rf INT, prob_highlight_nn INT, pred_highlight_nn INT);
 
 
--- ALTER TABLE clips ADD word TEXT;
--- ALTER TABLE labelled ADD word TEXT;
+ALTER TABLE clips ADD word TEXT;
+ALTER TABLE labelled ADD word TEXT;
 
 
 ALTER TABLE clips ADD subjectivity TEXT, ADD polarity TEXT, ADD pred_excitement TEXT;
